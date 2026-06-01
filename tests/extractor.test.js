aa1b3a98dd0a64f30e413c459692516a98f5f42e
Gabriel CG <gabrielcgs12@gmail.com>
2026-06-01 18:07:13 -0300

    chore: commit modified extractor, manifest and extractor tests

--- tests/extractor.test.js
+++ tests/extractor.test.js
@@ -1,5 +1,5 @@
 // tests/extractor.test.js
-const { extractProjectData, extractTramites, buildCsv } = require('../lib/extractor');
+const { extractProjectData } = require('../lib/extractor');
 
 function makeDoc(html) {
   document.body.innerHTML = html;
@@ -7,7 +7,7 @@ function makeDoc(html) {
 }
 
 describe('extractProjectData', () => {
-  test('extrai CAAE, título, pesquisador, situação', () => {
+  test('extrai CAAE, título, pesquisador, situação, área temática, patrocinador', () => {
     makeDoc(`
       <table>
         <tr><td><b><span class="labelClass">CAAE: </span></b>57957522.5.1001.5262</td></tr>
@@ -15,6 +15,8 @@ describe('extractProjectData', () => {
         <tr><td><b><span class="labelClass">Pesquisador Responsável: </span></b>João Silva</td></tr>
         <tr><td><b><span class="labelClass">Situação da Versão do Projeto: </span></b>Aprovado</td></tr>
         <tr><td><b><span class="labelClass">Instituição Proponente: </span></b>FIOCRUZ</td></tr>
+        <tr><td><b><span class="labelClass">Área Temática: </span></b>Genética Humana</td></tr>
+        <tr><td><b><span class="labelClass">Patrocinador Principal: </span></b>MS/SVS/DECIT</td></tr>
       </table>
     `);
     const data = extractProjectData(document);
@@ -23,6 +25,8 @@ describe('extractProjectData', () => {
     expect(data.pesquisador).toBe('João Silva');
     expect(data.situacao).toBe('Aprovado');
     expect(data.instituicao).toBe('FIOCRUZ');
+    expect(data.areaTematica).toBe('Genética Humana');
+    expect(data.patrocinador).toBe('MS/SVS/DECIT');
   });
 
   test('retorna null em campo ausente', () => {
@@ -32,44 +36,3 @@ describe('extractProjectData', () => {
   });
 });
 
-describe('extractTramites', () => {
-  test('extrai linhas da tabela de trâmites', () => {
-    makeDoc(`
-      <table id="formDetalharProjeto:tableTramiteApreciacaoProjeto">
-        <tbody id="formDetalharProjeto:tableTramiteApreciacaoProjeto:tb">
-          <tr class="rich-table-row">
-            <td><span>N22</span></td>
-            <td><span>26/05/2026 10:10:10</span></td>
-            <td><span>Parecer liberado</span></td>
-            <td><span>10</span></td>
-            <td><span>Coordenador</span></td>
-            <td><span>FIOCRUZ</span></td>
-            <td><span>CONEP</span></td>
-            <td><span></span></td>
-          </tr>
-        </tbody>
-      </table>
-    `);
-    const tramites = extractTramites(document);
-    expect(tramites).toHaveLength(1);
-    expect(tramites[0].apreciacao).toBe('N22');
-    expect(tramites[0].tipo).toBe('Parecer liberado');
-    expect(tramites[0].origem).toBe('FIOCRUZ');
-  });
-
-  test('retorna [] se tabela ausente', () => {
-    makeDoc('<p>sem tabela</p>');
-    expect(extractTramites(document)).toEqual([]);
-  });
-});
-
-describe('buildCsv', () => {
-  test('gera CSV com header + linhas', () => {
-    const tramites = [
-      { apreciacao: 'N22', dataHora: '26/05/2026', tipo: 'Parecer liberado', versao: '10', perfil: 'Coord', origem: 'FIOCRUZ', destino: 'CONEP', informacoes: '' }
-    ];
-    const csv = buildCsv(tramites);
-    expect(csv).toContain('Apreciação,Data/Hora,Tipo Trâmite');
-    expect(csv).toContain('N22,26/05/2026,Parecer liberado');
-  });
-});
